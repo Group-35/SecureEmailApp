25054173c87f67a605301d0fcacaa9cddd0c79ff
Thomas Tavarez <thomasiscooly@gmail.com>
2015-12-02 18:21:26 -0500

    added saving emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,13 +1,17 @@
 class EmailsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def new
     @email = Email.new
   end
 
   def create
-    to_user =  User.find_by(username: params[:to_user])
-    @email = Email.create(to_user: to_user.id, from_user: current_user.id, data: params[:data])
-    redirect_to 'home#index'
+
+    to_user =  User.find_by(username: params[:email][:to_user])
+    
+    @email = Email.create(to_user: to_user.id, from_user: current_user.id, data: params[:email][:data])
+    redirect_to :controller => 'home', :action => 'index'
   end
 
   def retrieve_email
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
-    @emails = ["this", "is", "a", "test"]
+    @emails = [Email.find_by(to_user: current_user.id)].to_a
   end
   
 end
