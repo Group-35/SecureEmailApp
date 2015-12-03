7cd677934f2e1d135d3a36f97d507040e352aeac
Thomas Tavarez <thomasiscooly@gmail.com>
2015-12-03 13:00:46 -0500

    set textarea to disabled

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,25 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @emails = Email.where(to_user: current_user.id)
+    @good_emails = Email.where(to_user: current_user.id)
+    @emails = @good_emails
+    @spam_users = Spam.where(belong_to: current_user.id)
+
+    @bad_emails = []
+
+    @emails.each do |e|
+      @spam_users.each do |s|
+        if (e.from_user == s.bad_user)
+          @bad_emails << e
+          break
+        end
+      end
+    end
+
+    @bad_emails.each do |b|
+      @emails.delete(b)
+    end
+
   end
   
 end
