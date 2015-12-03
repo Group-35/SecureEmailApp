ab80499b0a6cb86e1d89b0e9b70c9f1c63501b84
Thomas Tavarez <thomasiscooly@gmail.com>
2015-12-02 23:29:25 -0500

    added spam list

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -13,5 +13,18 @@ class EmailsController < ApplicationController
     @email = Email.create(to_user: to_user.id, from_user: current_user.id, data: params[:email][:data], created_at: DateTime.now, updated_at: DateTime.now)
     redirect_to :controller => 'home', :action => 'index'
   end
+
+  def show
+    emails = Email.where(to_user: current_user.id)
+    @email = emails.first
+  end
+
+  def show_spam
+    render 'spam'
+  end
+
+  def new_spam
+    render 'new_spam'
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
+  get "emails/spam" => 'emails#show_spam'
+  get "emails/spam/new" => 'email#new_spam'
+
   resources :emails
   resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
